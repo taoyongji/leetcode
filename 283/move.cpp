8ed14901d19044753fd5f8996dea8a1824149458
taoyongji <172230589@qq.com>
2020-11-19 13:42:43 +0800

    topic:283

---
+++ 283/move.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2020/11/19.
+//
+
+#include "move.h"
---
+++ 283/move.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2020/11/19.
+//
+
+#ifndef LEETCODE_MOVE_H
+#define LEETCODE_MOVE_H
+
+
+class move {
+
+};
+
+
+#endif //LEETCODE_MOVE_H
