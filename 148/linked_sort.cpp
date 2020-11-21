670cf97dbab953d2f4e764b87edb546c3a0bb4d8
taoyongji <172230589@qq.com>
2020-11-21 21:29:13 +0800

    terrible ^_^

---
+++ 148/linked_sort.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tyj on 2020/11/21.
+//
+
+#include "linked_sort.h"
---
+++ 148/linked_sort.h
@@ -0,0 +1,14 @@
+//
+// Created by tyj on 2020/11/21.
+//
+
+#ifndef LEETCODE_LINKED_SORT_H
+#define LEETCODE_LINKED_SORT_H
+
+
+class linked_sort {
+
+};
+
+
+#endif //LEETCODE_LINKED_SORT_H
